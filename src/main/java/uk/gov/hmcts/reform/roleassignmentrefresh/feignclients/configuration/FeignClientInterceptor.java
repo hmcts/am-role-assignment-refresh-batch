731705e6285e5c5aee82584c003c6c850b9d62cf
Nitish Singla <nitish.singla@hmcts.net>
2021-05-14 13:27:34 +0100

    AM-1339: Fixed the token generation issue.

--- src/main/java/uk/gov/hmcts/reform/roleassignmentrefresh/feignclients/configuration/FeignClientInterceptor.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignmentrefresh/feignclients/configuration/FeignClientInterceptor.java
@@ -1,21 +1,24 @@
 package uk.gov.hmcts.reform.roleassignmentrefresh.feignclients.configuration;
 
 import feign.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.roleassignmentrefresh.oidc.SecurityUtils;
 
 @Service
 public class FeignClientInterceptor {
 
+    @Autowired
+    SecurityUtils securityUtils;
+
     @Bean
     public RequestInterceptor requestInterceptor() {
         return requestTemplate -> {
-            if (!requestTemplate.url().contains("health")) {
-                requestTemplate.header("ServiceAuthorization", "Bearer ");
-                requestTemplate.header(HttpHeaders.AUTHORIZATION, "Bearer ");
-                requestTemplate.header(HttpHeaders.CONTENT_TYPE, "application/json");
-            }
+            requestTemplate.header("ServiceAuthorization", "Bearer " + securityUtils.getServiceToken());
+            requestTemplate.header(HttpHeaders.AUTHORIZATION, "Bearer " + securityUtils.getUserToken());
+            requestTemplate.header(HttpHeaders.CONTENT_TYPE, "application/json");
         };
     }
 

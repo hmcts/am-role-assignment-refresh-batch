b6bdaf36104277eba3bc1b558e0791bdd930bde8
Nitin <nitin.sahu@hmcts.net>
2021-05-24 11:00:59 +0100

    Integration tests for refresh batch

--- src/integrationTest/java/uk/gov/hmcts/reform/roleassignmentrefresh/BaseTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/roleassignmentrefresh/BaseTest.java
@@ -1,13 +1,26 @@
 package uk.gov.hmcts.reform.roleassignmentrefresh;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.opentable.db.postgres.embedded.EmbeddedPostgres;
 import org.junit.BeforeClass;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+
+import javax.annotation.PreDestroy;
+import javax.sql.DataSource;
 
 @Configuration
 public abstract class BaseTest {
@@ -25,5 +38,38 @@ public abstract class BaseTest {
             MediaType.APPLICATION_JSON.getSubtype(),
             StandardCharsets.UTF_8
     );
+
+    @TestConfiguration
+    static class Configuration {
+        Connection connection;
+
+        @Bean
+        public EmbeddedPostgres embeddedPostgres() throws IOException {
+            return EmbeddedPostgres
+                    .builder()
+                    .setPort(0)
+                    .start();
+        }
+
+        @Bean
+        public DataSource dataSource(@Autowired EmbeddedPostgres pg) throws Exception {
+
+            final Properties props = new Properties();
+            // Instruct JDBC to accept JSON string for JSONB
+            props.setProperty("stringtype", "unspecified");
+            connection = DriverManager.getConnection(pg.getJdbcUrl("postgres", "postgres"), props);
+            DataSource datasource = new SingleConnectionDataSource(connection, true);
+            return datasource;
+        }
+
+
+
+        @PreDestroy
+        public void contextDestroyed() throws SQLException {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
 
 }

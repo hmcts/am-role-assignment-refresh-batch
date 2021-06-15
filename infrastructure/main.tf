4fed26e09b4e55e0716520bbf69e3ef36e6a7deb
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2021-06-15 14:02:05 +0100

    Fixed RG name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,16 +1,16 @@
 locals {
-  preview_vault_name      = join("-", [var.raw_product, "aat"])
-  non_preview_vault_name  = join("-", [var.raw_product, var.env])
-  key_vault_name          = var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name
+  am_rg_name_prefix     = join("-", [var.raw_product, "shared-infrastructure"])
+  am_key_vault_rg_name  = var.env == "preview" || var.env == "spreview" ? join("-", [local.am_rg_name_prefix, "aat"]) : join("-", [local.am_rg_name_prefix, var.env])
+  am_key_vault_name     = var.env == "preview" || var.env == "spreview" ? join("-", [var.raw_product, "aat"]) : join("-", [var.raw_product, var.env])
 
-  s2s_rg_prefix               = "rpe-service-auth-provider"
-  s2s_key_vault_name          = var.env == "preview" || var.env == "spreview" ? join("-", ["s2s", "aat"]) : join("-", ["s2s", var.env])
-  s2s_vault_resource_group    = var.env == "preview" || var.env == "spreview" ? join("-", [local.s2s_rg_prefix, "aat"]) : join("-", [local.s2s_rg_prefix, var.env])
+  s2s_rg_prefix             = "rpe-service-auth-provider"
+  s2s_vault_resource_group  = var.env == "preview" || var.env == "spreview" ? join("-", [local.s2s_rg_prefix, "aat"]) : join("-", [local.s2s_rg_prefix, var.env])
+  s2s_key_vault_name        = var.env == "preview" || var.env == "spreview" ? join("-", ["s2s", "aat"]) : join("-", ["s2s", var.env])
 }
 
 data "azurerm_key_vault" "am_key_vault" {
   name                = local.key_vault_name
-  resource_group_name = local.key_vault_name
+  resource_group_name = local.am_key_vault_rg_name
 }
 
 data "azurerm_key_vault" "s2s_key_vault" {

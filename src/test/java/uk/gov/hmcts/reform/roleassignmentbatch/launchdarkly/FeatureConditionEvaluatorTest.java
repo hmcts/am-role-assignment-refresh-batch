0c45c003c3ca1057158005d4445dbebb5043cc01
“stanlykumar” <stanly.sambathkumar@HMCTS.NET>
2021-06-21 12:15:57 +0100

    Junit coverage improvements

--- src/test/java/uk/gov/hmcts/reform/roleassignmentbatch/launchdarkly/FeatureConditionEvaluatorTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignmentbatch/launchdarkly/FeatureConditionEvaluatorTest.java
@@ -1,8 +1,5 @@
 package uk.gov.hmcts.reform.roleassignmentbatch.launchdarkly;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
 import com.launchdarkly.sdk.server.LDClient;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,14 +8,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.repeat.RepeatStatus;
-import uk.gov.hmcts.reform.roleassignmentrefresh.advice.exception.ForbiddenException;
-import uk.gov.hmcts.reform.roleassignmentrefresh.domain.service.process.RefreshJobsOrchestrator;
 import uk.gov.hmcts.reform.roleassignmentrefresh.launchdarkly.FeatureConditionEvaluator;
-import uk.gov.hmcts.reform.roleassignmentrefresh.task.RefreshORMRules;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.Mockito.mock;
@@ -28,32 +21,14 @@ import static org.mockito.Mockito.when;
 public class FeatureConditionEvaluatorTest {
 
     @Mock
-    private RefreshJobsOrchestrator refreshJobsOrchestrator;
-
-    @Mock
     LDClient ldClient = mock(LDClient.class);
 
-    @Mock
-    FeatureConditionEvaluator featureConditionEvaluator = mock(FeatureConditionEvaluator.class);
-
     @InjectMocks
-    RefreshORMRules refreshORMRules;
+    FeatureConditionEvaluator featureConditionEvaluator;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-    }
-
-    @Test
-    public void verifyBatchJobFromTaskTest() {
-        when(featureConditionEvaluator.isFlagEnabled(any(), any())).thenReturn(true);
-        RepeatStatus status = refreshORMRules.execute(mock(StepContribution.class), mock(ChunkContext.class));
-        assertEquals(status, RepeatStatus.FINISHED);
-    }
-
-    @Test(expected = ForbiddenException.class)
-    public void getExceptionForFlagFromJob() {
-        refreshORMRules.execute(mock(StepContribution.class), mock(ChunkContext.class));
     }
 
     @Test

0a26d6a1d240a6cd08ba3520fab890d7231754ac
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2023-04-17 13:04:17 +0100

    AM-2750: - Upgrade App Insights to use v3 - Use Telemetry Flush method at end of execution

--- src/main/java/uk/gov/hmcts/reform/roleassignmentrefresh/RoleAssignmentRefreshApplication.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignmentrefresh/RoleAssignmentRefreshApplication.java
@@ -1,7 +1,9 @@
 package uk.gov.hmcts.reform.roleassignmentrefresh;
 
+import com.microsoft.applicationinsights.TelemetryClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -17,14 +19,23 @@ public class RoleAssignmentRefreshApplication {
 
     private static final Logger log = LoggerFactory.getLogger(RoleAssignmentRefreshApplication.class);
 
+    @Autowired
+    private static TelemetryClient client;
+
+    @Autowired
+    public RoleAssignmentRefreshApplication(TelemetryClient client) {
+        RoleAssignmentRefreshApplication.client = client;
+    }
     public static void main(String[] args) throws Exception {
         ApplicationContext context = SpringApplication.run(RoleAssignmentRefreshApplication.class, args);
-        //Sleep added to allow app-insights to flush the logs
-        Thread.sleep(1000 * 8);
+
         int exitCode = SpringApplication.exit(context);
         String exitCodeLog = String.format("RoleAssignmentRefreshApplication Application exiting with exit code %s",
                 exitCode);
         log.info(exitCodeLog);
+        client.flush();
+        //Sleep added to allow app-insights to flush the logs
+        Thread.sleep(1000 * 8);
         System.exit(exitCode);
     }
 }

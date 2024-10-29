33eabe1d74d5dc0283ef538d0a51137f082a58aa
mikebrownccd <104495891+mikebrownccd@users.noreply.github.com>
2024-10-29 13:43:03 +0000

    DTSAM-570 Email Refresh Counts (#876)
    * DTSAM-570 Email Refresh Counts - First Commit
    
    * role-assignment-refresh-batch-EMAIL-LIST secret added to chart, EMAIL_ENABLED added to config, commented code removed, EMAIL_SUBJECT updated,
    
    * Bumping chart version/ fixing aliases
    
    * remove sendgrid.mail.to from application-test.yaml
    
    * email removed from test-application, gradle thymeleaf version var created, runId removed from EmailData, templateMap moved and captor used for test verify, REFRESH_ENV added to chart
    
    * before count difference now pre populated with negative version of count value, tests updated, test renamed
    
    * Empty-Commit
    
    * JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES set true to try to fix json parsing error in demo
    
    * refactored to cast count response to new CountResponse instead of JsonNode
    
    * EmailConfiguration class added, spring-context-support gradle dependency added
    
    * am-sendgrid-api-key added to chart keyvault
    
    * remove unused code after big refactor
    
    * unused imports removed
    
    * start and finish refresh job DateTimes added to count email
    
    * afterRefreshTime moved to after triggerRASUserCount()
    
    * 2nd attempt at using beforeRefreshTime and afterRefreshTime in email template
    
    ---------
    
    Co-authored-by: hmcts-jenkins-a-to-c <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>
    Co-authored-by: Matt Nayler <57350764+mattnayler@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/roleassignmentrefresh/domain/service/process/RefreshJobsOrchestrator.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignmentrefresh/domain/service/process/RefreshJobsOrchestrator.java
@@ -9,21 +9,38 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.roleassignmentrefresh.advice.exception.UnprocessableEntityException;
 import uk.gov.hmcts.reform.roleassignmentrefresh.data.RefreshJobEntity;
+import uk.gov.hmcts.reform.roleassignmentrefresh.domain.model.Count;
+import uk.gov.hmcts.reform.roleassignmentrefresh.domain.model.CountResponse;
+import uk.gov.hmcts.reform.roleassignmentrefresh.domain.model.EmailData;
+import uk.gov.hmcts.reform.roleassignmentrefresh.domain.model.RefreshJob;
 import uk.gov.hmcts.reform.roleassignmentrefresh.domain.model.UserRequest;
+import uk.gov.hmcts.reform.roleassignmentrefresh.domain.service.common.EmailService;
 import uk.gov.hmcts.reform.roleassignmentrefresh.domain.service.common.PersistenceService;
 import uk.gov.hmcts.reform.roleassignmentrefresh.domain.service.common.UserCountService;
 import uk.gov.hmcts.reform.roleassignmentrefresh.domain.service.common.SendJobDetailsService;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @Slf4j
 @Service
 public class RefreshJobsOrchestrator {
+    public static final String EMAIL_SUBJECT = "Refresh Job Task";
+    public static final String ORG_USER_COUNT_BY_JURISDICTION = "OrgUserCountByJurisdiction";
+    public static final String ORG_USER_COUNT_BY_JURISDICTION_AND_ROLE_NAME = "OrgUserCountByJurisdictionAndRoleName";
+    public static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss z Z";
 
     private final PersistenceService persistenceService;
     private final SendJobDetailsService jobDetailsService;
     private final UserCountService userCountService;
+    private final EmailService emailService;
 
     @Value("${refresh-job-delay-duration}")
     private long refreshJobDelayDuration;
@@ -34,10 +51,12 @@ public class RefreshJobsOrchestrator {
     @Autowired
     public RefreshJobsOrchestrator(PersistenceService persistenceService,
                                    SendJobDetailsService jobDetailsService,
-                                   UserCountService userCountService) {
+                                   UserCountService userCountService,
+                                   EmailService emailService) {
         this.persistenceService = persistenceService;
         this.jobDetailsService = jobDetailsService;
         this.userCountService = userCountService;
+        this.emailService = emailService;
 
     }
 
@@ -55,7 +74,9 @@ public class RefreshJobsOrchestrator {
 
         } else {
             log.info("Calling RAS User Count Before Refresh");
-            triggerRASUserCount();
+            String beforeRefreshTime = ZonedDateTime.now(ZoneId.systemDefault())
+                    .format(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
+            final ResponseEntity<CountResponse> responseEntityBeforeRefresh = triggerRASUserCount();
 
             for (RefreshJobEntity job : jobs) {
                 runRefreshJob(job);
@@ -70,12 +91,34 @@ public class RefreshJobsOrchestrator {
             refreshJobDelay(refreshJobCountDelayDuration);
 
             log.info("Calling RAS User Count After Refresh");
-            triggerRASUserCount();
+            final ResponseEntity<CountResponse> responseEntityAfterRefresh = triggerRASUserCount();
+            String afterRefreshTime = ZonedDateTime.now(ZoneId.systemDefault())
+                    .format(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
+
+            if (responseEntityBeforeRefresh.getBody() != null && responseEntityAfterRefresh.getBody() != null) {
+                final CountResponse responseBeforeRefresh = responseEntityBeforeRefresh.getBody();
+                final CountResponse responseAfterRefresh = responseEntityAfterRefresh.getBody();
+                List<RefreshJob> refreshJobs = populateRefreshJobs(jobs);
+
+                sendEmailWithCounts(responseBeforeRefresh, responseAfterRefresh, refreshJobs, beforeRefreshTime,
+                        afterRefreshTime);
+            }
+
         }
 
         log.info(" >> Refresh Batch Job({}) execution finished at {} . Time taken = {} milliseconds",
                 jobs.size(), System.currentTimeMillis(), Math.subtractExact(System.currentTimeMillis(), startTime)
         );
+    }
+
+    public List<RefreshJob> populateRefreshJobs(List<RefreshJobEntity> jobs) {
+        return jobs.stream()
+                .map(jobEntity -> RefreshJob.builder()
+                        .jobId(jobEntity.getJobId().toString())
+                        .jurisdiction(jobEntity.getJurisdiction())
+                        .roleCategory(jobEntity.getRoleCategory())
+                        .build())
+                .toList();
     }
 
     private void runRefreshJob(RefreshJobEntity job) {
@@ -109,11 +152,121 @@ public class RefreshJobsOrchestrator {
         }
     }
 
-    public void triggerRASUserCount() {
-        ResponseEntity<Object> responseEntity =  userCountService.getRasUserCounts();
+    public ResponseEntity<CountResponse> triggerRASUserCount() {
+        ResponseEntity<CountResponse> responseEntity =  userCountService.getRasUserCounts();
         if (responseEntity.getStatusCode() != HttpStatus.OK) {
             log.error("Error return from RAS User Count: " + responseEntity.toString());
         }
+        return responseEntity;
     }
 
+
+    public List<Count> compareCounts(CountResponse before, CountResponse after, String countName) {
+        List<Count> countOutput = new ArrayList<>();
+
+        List<CountResponse.CountData> beforeList;
+        if (ORG_USER_COUNT_BY_JURISDICTION.equals(countName)) {
+            beforeList = before.getOrgUserCountByJurisdiction() != null
+                    ? List.of(before.getOrgUserCountByJurisdiction()) : new ArrayList<>();
+        } else {
+            beforeList = before.getOrgUserCountByJurisdictionAndRoleName() != null
+                    ? List.of(before.getOrgUserCountByJurisdictionAndRoleName()) : new ArrayList<>();
+        }
+
+        for (CountResponse.CountData countData : beforeList) {
+            Count count = new Count();
+            count.populateBefore(countData);
+            countOutput.add(count);
+        }
+
+        List<CountResponse.CountData> afterList;
+        if (ORG_USER_COUNT_BY_JURISDICTION.equals(countName)) {
+            afterList = after.getOrgUserCountByJurisdiction() != null
+                    ? List.of(after.getOrgUserCountByJurisdiction()) : new ArrayList<>();
+        } else {
+            afterList = after.getOrgUserCountByJurisdictionAndRoleName() != null
+                    ? List.of(after.getOrgUserCountByJurisdictionAndRoleName()) : new ArrayList<>();
+        }
+
+        for (CountResponse.CountData countData : afterList) {
+            Count count = new Count();
+            count.populateAfter(countData);
+            updateWithAfterCountOrCreateNew(count, countOutput);
+        }
+
+        countOutput.sort(Comparator
+                .comparing(Count::getJurisdiction, Comparator.nullsFirst(String::compareTo))
+                .thenComparing(Count::getRoleCategory, Comparator.nullsFirst(String::compareTo))
+                .thenComparing(Count::getRoleName, Comparator.nullsFirst(String::compareTo)));
+
+        return countOutput;
+    }
+
+    public Count findMatchingCount(Count needle, List<Count> haystack) {
+        Count returnCount = null;
+
+        for (Count count : haystack) {
+            boolean jurisdictionMatch = isJurisdictionMatch(count, needle);
+            boolean roleNameMatch = isRoleNameMatch(count, needle);
+            boolean roleCategoryMatch = count.getRoleCategory().equals(needle.getRoleCategory());
+
+            if (jurisdictionMatch && roleCategoryMatch && roleNameMatch) {
+                returnCount = count;
+            }
+        }
+        return returnCount;
+    }
+
+    boolean isJurisdictionMatch(Count count, Count needle) {
+        if (count.getJurisdiction() == null && needle.getJurisdiction() == null) {
+            return true;
+        } else if (count.getJurisdiction() != null && needle.getJurisdiction() != null) {
+            return count.getJurisdiction().equals(needle.getJurisdiction());
+        } else {
+            return false;
+        }
+    }
+
+    boolean isRoleNameMatch(Count count, Count needle) {
+        if (count.getRoleName() == null && needle.getRoleName() == null) {
+            return true;
+        } else if (count.getRoleName() != null && needle.getRoleName() != null) {
+            return count.getRoleName().equals(needle.getRoleName());
+        } else {
+            return false;
+        }
+    }
+
+    public void updateWithAfterCountOrCreateNew(Count afterCount, List<Count> counts) {
+        Count matchedCount = findMatchingCount(afterCount, counts);
+        if (matchedCount != null) {
+            matchedCount.updateAfterCount(afterCount.getAfterCount());
+        } else {
+            afterCount.setDifference(afterCount.getAfterCount());
+            counts.add(afterCount);
+        }
+    }
+
+    public void sendEmailWithCounts(CountResponse responseBeforeRefresh, CountResponse responseAfterRefresh,
+                                    List<RefreshJob> refreshJobs, String beforeRefreshTime, String afterRefreshTime) {
+        List<Count> jurisdictionCount = compareCounts(responseBeforeRefresh, responseAfterRefresh,
+                ORG_USER_COUNT_BY_JURISDICTION);
+        List<Count> jurisdictionAndRoleNameCount = compareCounts(responseBeforeRefresh, responseAfterRefresh,
+                ORG_USER_COUNT_BY_JURISDICTION_AND_ROLE_NAME);
+
+        Map<String, Object> templateMap = new HashMap<>();
+
+        templateMap.put("refreshJobs", refreshJobs);
+        templateMap.put("jurisdictionCount", jurisdictionCount);
+        templateMap.put("jurisdictionAndRoleNameCount", jurisdictionAndRoleNameCount);
+        templateMap.put("beforeRefreshTime", beforeRefreshTime);
+        templateMap.put("afterRefreshTime", afterRefreshTime);
+
+        EmailData emailData = EmailData
+                .builder()
+                .emailSubject(EMAIL_SUBJECT)
+                .templateMap(templateMap)
+                .build();
+        emailService.sendEmail(emailData);
+    }
 }
